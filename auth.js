a508c285382385cb61c684ef0eef87fa88c40713
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-04 11:46:53 +0200

    Implementation of Social Authentication II

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const bcrypt = require('bcrypt');
 const ObjectID = require('mongodb').ObjectID;
+const GitHubStrategy = require('passport-github').Strategy;
 
 module.exports = function (app, myDataBase) {
   // Serialization and deserialization here...
@@ -31,5 +32,20 @@ module.exports = function (app, myDataBase) {
         return done(null, user);
       });
     })
+  );
+
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL:
+          'https://hattila-fcc-advancednode.herokuapp.com/auth/github/callback',
+      },
+      function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        // Database logic here with callback containing our user object
+      }
+    )
   );
 };
