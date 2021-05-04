9c45782706cebb3c4d2fe10caa1cb396da0b2098
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-04 09:25:16 +0200

    Logging a User Out

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 'use strict';
 require('dotenv').config();
-
 const express = require('express');
-const session = require('express-session');
-const passport = require('passport');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
-const app = express();
 const LocalStrategy = require('passport-local');
 
-fccTesting(app); //For FCC testing purposes
+const app = express();
+fccTesting(app); // For fCC testing purposes
+
+app.set('view engine', 'pug');
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,6 +29,8 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+/********************************************************************************************** */
+
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
@@ -37,7 +41,6 @@ myDB(async client => {
       title: 'Connected to Database',
       message: 'Please login',
       showLogin: true,
-      username: req.user.username,
     });
   });
 
@@ -51,17 +54,21 @@ myDB(async client => {
     );
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render(process.cwd() + '/views/pug/profile');
-  });
-
-  app.set('view engine', 'pug');
-
-  app.route('/').get((req, res) => {
-    res.render(process.cwd() + '/views/pug/index.pug', {
-      title: 'Hello',
-      message: 'Please login',
+    res.render(process.cwd() + '/views/pug/profile', {
+      username: req.user.username,
     });
   });
+
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
+  });
+
+  /********************************************************************************************** */
 
   // Serialization and deserialization here...
   passport.serializeUser((user, done) => {
@@ -73,11 +80,9 @@ myDB(async client => {
     });
   });
 
-  // https://www.passportjs.org/packages/passport-local/
   passport.use(
     new LocalStrategy(function (username, password, done) {
       myDataBase.findOne({ username: username }, function (err, user) {
-        console.log(user);
         console.log('User ' + username + ' attempted to log in.');
         if (err) {
           return done(err);
@@ -92,7 +97,6 @@ myDB(async client => {
       });
     })
   );
-
   // Be sure to add this...
 }).catch(e => {
   app.route('/').get((req, res) => {
@@ -108,7 +112,7 @@ function ensureAuthenticated(req, res, next) {
 }
 
 // app.listen out here...
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port ' + process.env.PORT);
 });
