145505ea97f1f762f0755f862b9bd1218df11c70
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-04 12:21:49 +0200

    Implementation of Social Authentication II remove newlines

--- server.js
+++ server.js
@@ -37,7 +37,88 @@ myDB(async client => {
 
   /********************************************************************************************** */
 
-  routes(app, myDataBase);
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('pug', {
+      title: 'Connected to Database',
+      message: 'Please login',
+      showLogin: true,
+      showRegistration: true,
+      showSocialAuth: true,
+    });
+  });
+
+  app
+    .route('/login')
+    .post(
+      passport.authenticate('local', { failureRedirect: '/' }),
+      (req, res) => {
+        res.redirect('/profile');
+      }
+    );
+
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    res.render(process.cwd() + '/views/pug/profile', {
+      username: req.user.username,
+    });
+  });
+
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.route('/register').post(
+    (req, res, next) => {
+      const hash = bcrypt.hashSync(req.body.password, 12);
+
+      myDataBase.findOne({ username: req.body.username }, function (err, user) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne(
+            { username: req.body.username, password: hash },
+            (err, doc) => {
+              if (err) {
+                res.redirect('/');
+              } else {
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
+
+  /*******************GITHUB**************************/
+  app.route('/auth/github').get(passport.authenticate('github'));
+
+  app.route('/auth/github/callback')
+      .get(passport.authenticate('github', {failureRedirect: '/'}),
+      (req, res) => {
+        res.redirect('/profile');
+    });
+
+      
+  /*******************GITHUB**************************/
+
+  app.use((req, res, next) => {
+    res.status(404).type('text').send('Not Found');
+  });
+
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/');
+  }
 
   auth(app, myDataBase);
 
