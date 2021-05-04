5793bad0115583ed2b0ae91b20cb22ccc8827038
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-04 09:40:27 +0200

    Registration of New Users

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ myDB(async client => {
       title: 'Connected to Database',
       message: 'Please login',
       showLogin: true,
+      showRegistration: true,
     });
   });
 
@@ -63,6 +64,33 @@ myDB(async client => {
     req.logout();
     res.redirect('/');
   });
+
+  app.route('/register').post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, function (err, user) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne(
+            { username: req.body.username, password: req.body.password },
+            (err, doc) => {
+              if (err) {
+                res.redirect('/');
+              } else {
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
 
   app.use((req, res, next) => {
     res.status(404).type('text').send('Not Found');
